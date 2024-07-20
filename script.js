f85055c0668aa311a49ba3c84412abac5ba7d897
gchristiansun <167200722+gchristiansun@users.noreply.github.com>
2024-07-20 12:34:42 +0700

    add typing effect

--- script.js
+++ script.js
@@ -6,3 +6,39 @@ if (window.scrollY > 450) {
     header.classList.remove('hidden');
 }
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const typingElement = document.getElementById('typing');
+    const constantElement = document.getElementById('constant');
+    const textArray = ["Welcome to my website!", "I'm Gregorius Christian Sunaryo"];
+    let arrayIndex = 0;
+    let charIndex = 0;
+
+    function type() {
+        if (charIndex < textArray[arrayIndex].length) {
+            typingElement.textContent += textArray[arrayIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, 100);
+        } else {
+            setTimeout(erase, 2000);
+        }
+    }
+
+    function erase() {
+        if (charIndex > 0) {
+            typingElement.textContent = textArray[arrayIndex].substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(erase, 50);
+        } else {
+            arrayIndex = (arrayIndex + 1) % textArray.length;
+            setTimeout(type, 1000);
+        }
+    }
+    
+    if (window.innerWidth < 420) {
+        constantElement.style.display = 'none';
+        type();
+      } else {
+        typingElement.style.display = 'none';
+      }
+});
